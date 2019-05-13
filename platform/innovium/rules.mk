deecdc7cfc11fa67d013bb13944c304856cbb09f
Pradchaya P <pphuchar@celestica.com>
2019-05-13 20:21:35 +0800

    [platform/innovium] Add platform monitor and device config to one-image
     *The image now boot up with pmon and device configuration.
     *Please note that no SAI related dockers is added to image.

--- platform/innovium/rules.mk
+++ platform/innovium/rules.mk
@@ -1,10 +1,4 @@
 include $(PLATFORM_PATH)/platform-modules-cel.mk
 include $(PLATFORM_PATH)/one-image.mk
 
-SONIC_ALL += SONIC_ALL += $(DOCKER_DATABASE) \
-         $(DOCKER_LLDP_SV2) \
-         $(DOCKER_SNMP_SV2) \
-         $(DOCKER_PLATFORM_MONITOR) \
-         $(DOCKER_DHCP_RELAY) \
-         $(DOCKER_PTF) \
-         $(SONIC_GENERIC_ONIE_IMAGE)
+SONIC_ALL += $(SONIC_ONE_IMAGE) $(DOCKER_PTF)
